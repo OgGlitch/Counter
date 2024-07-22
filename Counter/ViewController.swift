771165409cfd9ba409296797ff36b57506262a94
OgGlitch <i@pis-dec.ru>
2024-07-22 07:18:39 +0500

    Basic task

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    
+    @IBOutlet weak var counterLable: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
+    
+    @IBAction func pressedButton() {
+        count += 1
+        counterLable.text = "Значение счётчика: \(count)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterLable.text = "Значение счётчика: \(count)"
+        counterButton.setTitle("Увеличить счетчик", for: UIControl.State.normal)
     }
-
-
 }
 
