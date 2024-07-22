e7f4117dd2f9fc870dc6f3fde8310996662cbe9e
OgGlitch <i@pis-dec.ru>
2024-07-22 10:57:01 +0500

    Complicated task

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,21 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var count = 0
+    private var count = 0
     
     @IBOutlet weak var counterLable: UILabel!
-    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterDecreaseButton: UIButton!
+    @IBOutlet weak var counterIncreaseButton: UIButton!
+    @IBOutlet weak var counterResetButton: UIButton!
+    @IBOutlet weak var historyTextView: UITextView!
     
     @IBAction func pressedButton() {
+        if count > 0 {
+            count -= 1
+            historyTextView.insertText(getCurrentDateAndTime() + ": значение изменено на -1\n")
+        } else {
+            historyTextView.insertText(getCurrentDateAndTime() + ": попытка уменьшить значение счётчика ниже 0\n")
+        }
+        displayCount()
+        scrollText()
+    }
+    @IBAction func pressIncreaseButton() {
         count += 1
-        counterLable.text = "Значение счётчика: \(count)"
+        displayCount()
+        historyTextView.insertText(getCurrentDateAndTime() + ": значение изменено на +1\n")
+        scrollText()
+    }
+    @IBAction func pressResetButton() {
+        count = 0
+        displayCount()
+        historyTextView.insertText(getCurrentDateAndTime() + ": значение сброшено\n")
+        scrollText()
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        displayCount()
+        counterDecreaseButton.setTitle("-", for: UIControl.State.normal)
+        counterIncreaseButton.setTitle("+", for: UIControl.State.normal)
+        counterIncreaseButton.tintColor = .red
+        counterResetButton.setTitle("↻", for: UIControl.State.normal)
+        counterResetButton.tintColor = .gray
+        historyTextView.text = "История изменений:\n"
+    }
+    
+    private func displayCount() {
         counterLable.text = "Значение счётчика: \(count)"
-        counterButton.setTitle("Увеличить счетчик", for: UIControl.State.normal)
+    }
+    
+    private func getCurrentDateAndTime() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.timeStyle = .short
+        dateFormatter.dateStyle = .short
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    private func scrollText() {
+        let range = NSRange(location: historyTextView.text.count - 1, length: 0)
+        historyTextView.scrollRangeToVisible(range)
     }
 }
 
