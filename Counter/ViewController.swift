f7521e9656245579ac3b0e45794b2ffc1084ed79
OgGlitch <i@pis-dec.ru>
2024-07-24 10:22:20 +0500

    Access modifiers and order fixes

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,37 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet private weak var counterLable: UILabel!
+    @IBOutlet private weak var counterDecreaseButton: UIButton!
+    @IBOutlet private weak var counterIncreaseButton: UIButton!
+    @IBOutlet private weak var counterResetButton: UIButton!
+    @IBOutlet private weak var historyTextView: UITextView!
+    
     private var count = 0
-    
-    @IBOutlet weak var counterLable: UILabel!
-    @IBOutlet weak var counterDecreaseButton: UIButton!
-    @IBOutlet weak var counterIncreaseButton: UIButton!
-    @IBOutlet weak var counterResetButton: UIButton!
-    @IBOutlet weak var historyTextView: UITextView!
-    
-    @IBAction func pressedButton() {
-        if count > 0 {
-            count -= 1
-            historyTextView.insertText(getCurrentDateAndTime() + ": значение изменено на -1\n")
-        } else {
-            historyTextView.insertText(getCurrentDateAndTime() + ": попытка уменьшить значение счётчика ниже 0\n")
-        }
-        displayCount()
-        scrollText()
-    }
-    @IBAction func pressIncreaseButton() {
-        count += 1
-        displayCount()
-        historyTextView.insertText(getCurrentDateAndTime() + ": значение изменено на +1\n")
-        scrollText()
-    }
-    @IBAction func pressResetButton() {
-        count = 0
-        displayCount()
-        historyTextView.insertText(getCurrentDateAndTime() + ": значение сброшено\n")
-        scrollText()
-    }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,6 +26,29 @@ class ViewController: UIViewController {
         counterResetButton.setTitle("↻", for: UIControl.State.normal)
         counterResetButton.tintColor = .gray
         historyTextView.text = "История изменений:\n"
+    }
+    
+    @IBAction private func pressedButton() {
+        if count > 0 {
+            count -= 1
+            historyTextView.insertText(getCurrentDateAndTime() + ": значение изменено на -1\n")
+        } else {
+            historyTextView.insertText(getCurrentDateAndTime() + ": попытка уменьшить значение счётчика ниже 0\n")
+        }
+        displayCount()
+        scrollText()
+    }
+    @IBAction private func pressIncreaseButton() {
+        count += 1
+        displayCount()
+        historyTextView.insertText(getCurrentDateAndTime() + ": значение изменено на +1\n")
+        scrollText()
+    }
+    @IBAction private func pressResetButton() {
+        count = 0
+        displayCount()
+        historyTextView.insertText(getCurrentDateAndTime() + ": значение сброшено\n")
+        scrollText()
     }
     
     private func displayCount() {
